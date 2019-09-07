5d88388a5317228a243676cd7bb9c31d62a3a8af
Maarten de Rijk <maarten.derijk@gmail.com>
2019-09-07 15:56:24 +0200

    fixed paths

--- pi_sensor.py
+++ pi_sensor.py
@@ -11,6 +11,7 @@ now = datetime.now()
 datestr = now.strftime(r"%Y%m%d%H%M%S")
 img_url = "./docs/img/snap_" + datestr + ".jpg"
 camera = PiCamera()
+camera.resolution = (640, 480)
 camera.start_preview()
 sleep(2)
 camera.capture(img_url)
@@ -21,11 +22,11 @@ camera.stop_preview()
 now = datetime.now()
 snapshot_page = SiteGenerator()
 snapshot_page.base_template = "base_snapshot.html"
-snapshot_page.output_file = "./templates/snapshot_" + datestr + ".jpg"
+snapshot_page.output_file = "./templates/snapshot_" + datestr + ".html"
 snapshot_page.replacements = {
     "datetime": now.strftime(r"%Y-%m-%d %H:%M:%S"),
     "temperature": "24 C",
-    "img_url": img_url
+    "img_url": img_url.replace("/docs","")
 }
 snapshot_page.render()
 
