2de4a7c99f3dd17911e998886a63e2dcdad4f350
Maarten de Rijk <maarten.derijk@gmail.com>
2019-09-07 15:46:16 +0200

    raspberry pi photo functionality

--- pi_sensor.py
+++ pi_sensor.py
@@ -1,17 +1,31 @@
 ''' Automatic site update based on sensor data of raspberry pi with SENSE-hat'''
+from picamera import PiCamera
+from time import sleep
 from sense_hat import SenseHat
 from sitegenerator.sitegenerator import SiteGenerator
 from datetime import datetime
+
+
+# Get Pi Sensor data
+now = datetime.now()
+datestr = now.strftime(r"%Y%m%d%H%M%S")
+img_url = "./docs/img/snap_" + datestr + ".jpg"
+camera = PiCamera()
+camera.start_preview()
+sleep(2)
+camera.capture(img_url)
+camera.stop_preview()
 
 
 # Generate template for sensor data
 now = datetime.now()
 snapshot_page = SiteGenerator()
 snapshot_page.base_template = "base_snapshot.html"
-snapshot_page.output_file = "./templates/snapshot_" + now.strftime(r"%Y%m%d%H%M%S") + ".html"
+snapshot_page.output_file = "./templates/snapshot_" + datestr + ".jpg"
 snapshot_page.replacements = {
     "datetime": now.strftime(r"%Y-%m-%d %H:%M:%S"),
-    "temperature": "24 C"
+    "temperature": "24 C",
+    "img_url": img_url
 }
 snapshot_page.render()
 
